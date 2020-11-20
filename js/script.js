8256b816583c55c7005f44ef6f2291ff1cbd5038
João Gabriel <joaogabrielsg.00@gmail.com>
2020-11-20 14:25:06 -0300

    Update Animals API path

--- js/script.js
+++ js/script.js
@@ -37,6 +37,6 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
